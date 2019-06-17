b04faebbda3dfbf23f1f54c713f18817dd0b2b7b
George Paiva <george.paiva15@gmail.com>
2019-06-17 09:55:33 -0300

    Ajustando mascara de CPF.

--- dashboard.php
+++ dashboard.php
@@ -20,12 +20,12 @@ $user = $stmt->fetch(PDO::FETCH_ASSOC);
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Página Principal</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 
-       
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-    <!------ Include the above in your HEAD tag ---------->
+    <!------ Inclua o acima em sua tag HEAD ---------->
 
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -37,58 +37,61 @@ $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
         <!-- Sidebar -->
         <div class="bg-light border-right" id="sidebar-wrapper">
-          <div class="sidebar-heading">Olá, <?php echo $_SESSION['nome'] ?> <a href="inc/logout.php"><i class="fa fa-times-circle-o fa-lg"></a></i></div>
-          <div class="list-group list-group-flush">
-            <a href="usuarios.php" class="list-group-item list-group-item-action bg-light">Usuários</a>            
-            <a href="veiculos.php" class="list-group-item list-group-item-action bg-light">Veículos</a>            
-            <a href="#" class="list-group-item list-group-item-action bg-light">Teste</a>
-            <a href="#" class="list-group-item list-group-item-action bg-light">Relatórios</a>
-          </div>
+            <div class="sidebar-heading">Olá, <?php echo $_SESSION['nome'] ?> <a href="inc/logout.php"><i
+                        class="fa fa-times-circle-o fa-lg"></a></i></div>
+            <div class="list-group list-group-flush">
+                <a href="usuarios.php" class="list-group-item list-group-item-action bg-light">Usuários</a>
+                <a href="veiculos.php" class="list-group-item list-group-item-action bg-light">Veículos</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light">Teste</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light">Relatórios</a>
+            </div>
         </div>
         <!-- /#sidebar-wrapper -->
-    
+
         <!-- Page Content -->
         <div id="page-content-wrapper">
-    
-          <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-            <button class="btn btn-primary" id="menu-toggle">Menu</button>
-    
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-    
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-                <li class="nav-item active">
-                  <a class="nav-link" href="dashboard.php">Inicio <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="inc/logout.php">Logout</a>
-                </li>                
-              </ul>
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+                <button class="btn btn-primary" id="menu-toggle">Menu</button>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="dashboard.php">Inicio <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="inc/logout.php">Logout</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+            <div class="container-fluid" id="conteudo">
+                <h1 class="mt-4">Página em construção</h1>
+                <p>Locadora RMGD. Em construção.</p>
+                <p>Desculpe o incômodo, voltaremos em breve!</p>
             </div>
-          </nav>
-    
-          <div class="container-fluid" id="conteudo">
-            <h1 class="mt-4">Página em construção</h1>
-            <p>Locadora RMGD. Em construção.</p>
-            <p>Desculpe o incômodo, voltaremos em breve!</p>
-          </div>
         </div>
         <!-- /#page-content-wrapper -->
-    
-      </div>
-      <!-- /#wrapper -->
-    
-      
-    
-      <!-- Menu Toggle Script -->
-      <script>
-        $("#menu-toggle").click(function(e) {
-          e.preventDefault();
-          $("#wrapper").toggleClass("toggled");
-        });
-      </script>
+
+    </div>
+    <!-- /#wrapper -->
+
+
+
+    <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    </script>
 </body>
 
 </html>
--- usuarios.php
+++ usuarios.php
@@ -28,47 +28,49 @@ $lista = $stmt->fetchAll(PDO::FETCH_ASSOC);
             </div>
         </div>
 
-        
+
 
         <div class="row">
             <div class="col-md-8 col-sm-12 col-md-offset-3">
                 <table class="table table-hover">
                     <thead>
-                    <tr>
-                        <th>Nome</th>
-                        <th>CPF</th>
-                        <th>Perfil</th>
-                        <th>Email</th>
-                        <th>Status</th>
-                        <th>Alterar/Apagar</th>
-                        <th></th>
-                    </tr>
+                        <tr>
+                            <th>Nome</th>
+                            <th>CPF</th>
+                            <th>Perfil</th>
+                            <th>Email</th>
+                            <th>Status</th>
+                            <th>Alterar/Apagar</th>
+                            <th></th>
+                        </tr>
                     </thead>
                     <tbody>
-                    <?php foreach ($lista as $linha): ?>
+                        <?php foreach ($lista as $linha): ?>
                         <tr>
-                        <td><?php echo $linha['nome'] ?></td>
-                        <td><?php echo $linha['cpf'] ?></td>
-                        <td><?php echo $linha['Perfil'] ?></td>
-                        <td><?php echo $linha['email'] ?></td>
-                        
-                        <td><?php echo $linha['status'] ?></td>
-                        <td>
-                            <a href="usuario-alterar.php?id_usuario=<?php echo $linha['id_usuario'] ?>" class="btn btn-xs btn-info"><span class="glyphicon glyphicon-cog"></span></a>
-                            <a href="inc/excluir-usuario.php?id_usuario=<?php echo $linha['id_usuario']?>" class="btn btn-xs btn-danger"><span class="glyphicon glyphicon-trash"></span></a>
-                        </td>
+                            <td><?php echo $linha['nome'] ?></td>
+                            <td><?php echo $linha['cpf'] ?></td>
+                            <td><?php echo $linha['Perfil'] ?></td>
+                            <td><?php echo $linha['email'] ?></td>
+
+                            <td><?php echo $linha['status'] ?></td>
+                            <td>
+                                <a href="usuario-alterar.php?id_usuario=<?php echo $linha['id_usuario'] ?>"
+                                    class="btn btn-xs btn-info"><span class="glyphicon glyphicon-cog"></span></a>
+                                <a href="inc/excluir-usuario.php?id_usuario=<?php echo $linha['id_usuario']?>"
+                                    class="btn btn-xs btn-danger"><span class="glyphicon glyphicon-trash"></span></a>
+                            </td>
                         </tr>
-                    <?php endforeach ?>
+                        <?php endforeach ?>
                     </tbody>
                 </table>
-                </div>
             </div>
         </div>
-        <div class="row">
-            <div class=" col-md-3 col-sm-12 col-md-offset-3">
-                <a href="usuario-criar.php" class="btn btn-info btn-block" id="insert">Cadastrar Usuário</a>
-            </div>
-        </div>                    
+    </div>
+    <div class="row">
+        <div class=" col-md-3 col-sm-12 col-md-offset-3">
+            <a href="usuario-criar.php" class="btn btn-info btn-block" id="insert">Cadastrar Usuário</a>
+        </div>
+    </div>
     </div>
 
 </body>
