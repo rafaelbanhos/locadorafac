b5d14c9931219a888f17aa8f7884c7ba46747b9d
rafaelbanhos <rafaelbanhos@gmail.com>
2019-06-17 20:00:52 -0300

    dashboard ajustada usuarios

--- dashboard.php
+++ dashboard.php
@@ -2,13 +2,13 @@
 session_start();
 require_once('inc/conexao.php');
 
-if(!isset($_SESSION['login']) && !isset($_SESSION['senha'])):
-    header("Location: index.php");	
+if (!isset($_SESSION['login']) && !isset($_SESSION['senha'])) :
+  header("Location: index.php");
 endif;
 
-$pdo=Database::conexao();
+$pdo = Database::conexao();
 $stmt = $pdo->prepare("SELECT * FROM tb_usuario WHERE id_usuario=:id_usuario");
-$stmt->execute(['id_usuario' => $_SESSION['id_usuario']]); 
+$stmt->execute(['id_usuario' => $_SESSION['id_usuario']]);
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
 
@@ -17,113 +17,92 @@ $user = $stmt->fetch(PDO::FETCH_ASSOC);
 <html lang="pt-br">
 
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Página Principal</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Página Principal</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-    <!------ Inclua o acima em sua tag HEAD ---------->
+  <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+  <!------ Inclua o acima em sua tag HEAD ---------->
 
-    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/simple-sidebar.css" />
+  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" media="screen" href="css/simple-sidebar.css" />
 </head>
 
 <body>
-    <div class="d-flex" id="wrapper">
+  <div class="d-flex" id="wrapper">
 
-        <!-- Sidebar -->
-        <div class="bg-light border-right" id="sidebar-wrapper">
-<<<<<<< HEAD
-          <div class="sidebar-heading">Olá, <?php echo $_SESSION['nome'] ?> <a href="inc/logout.php"><i class="fa fa-times-circle-o fa-lg"></a></i></div>
-          <div class="list-group list-group-flush">
-            <a href="http://localhost/locadora/dashboard.php?p=usuarios.php" class="list-group-item list-group-item-action bg-light">Usuários</a>            
-            <a href="http://localhost/locadora/dashboard.php?p=veiculos.php" class="list-group-item list-group-item-action bg-light">Veículos</a>            
-            <a href="#" class="list-group-item list-group-item-action bg-light">Teste</a>
-            <a href="#" class="list-group-item list-group-item-action bg-light">Relatórios</a>
-          </div>
-=======
-            <div class="sidebar-heading">Olá, <?php echo $_SESSION['nome'] ?> <a href="inc/logout.php"><i
-                        class="fa fa-times-circle-o fa-lg"></a></i></div>
-            <div class="list-group list-group-flush">
-                <a href="usuarios.php" class="list-group-item list-group-item-action bg-light">Usuários</a>
-                <a href="veiculos.php" class="list-group-item list-group-item-action bg-light">Veículos</a>
-                <a href="#" class="list-group-item list-group-item-action bg-light">Teste</a>
-                <a href="#" class="list-group-item list-group-item-action bg-light">Relatórios</a>
-            </div>
->>>>>>> b04faebbda3dfbf23f1f54c713f18817dd0b2b7b
+    <!-- Sidebar -->
+    <div class="bg-light border-right" id="sidebar-wrapper">
+
+      <div class="sidebar-heading">Olá, <?php echo $_SESSION['nome'] ?> <a href="inc/logout.php"><i class="fa fa-times-circle-o fa-lg"></a></i></div>
+      <div class="list-group list-group-flush">
+        <a href="http://localhost/locadora/dashboard.php?p=usuarios.php" class="list-group-item list-group-item-action bg-light">Usuários</a>
+        <a href="http://localhost/locadora/dashboard.php?p=veiculos.php" class="list-group-item list-group-item-action bg-light">Veículos</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Teste</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Relatórios</a>
+      </div>
+
+    </div>
+    <!-- /#sidebar-wrapper -->
+
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+        <button class="btn btn-primary" id="menu-toggle">Menu</button>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+              <a class="nav-link" href="dashboard.php">Inicio <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="inc/logout.php">Logout</a>
+            </li>
+          </ul>
         </div>
-        <!-- /#sidebar-wrapper -->
+      </nav>
 
-        <!-- Page Content -->
-        <div id="page-content-wrapper">
 
-            <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-                <button class="btn btn-primary" id="menu-toggle">Menu</button>
+      </nav>
 
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="dashboard.php">Inicio <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="inc/logout.php">Logout</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-
-            <div class="container-fluid" id="conteudo">
-                <h1 class="mt-4">Página em construção</h1>
-                <p>Locadora RMGD. Em construção.</p>
-                <p>Desculpe o incômodo, voltaremos em breve!</p>
-            </div>
-<<<<<<< HEAD
-          </nav>
-    
-          <div class="container-fluid" id="conteudo">
-            <!--<h1 class="mt-4">Página em construção</h1>
+      <div class="container-fluid" id="conteudo">
+        <!--<h1 class="mt-4">Página em construção</h1>
             <p>Locadora RMGD. Em construção.</p>
             <p>Desculpe o incômodo, voltaremos em breve!</p>
 --><?php if (isset($_GET['p'])) {
-          include($_GET['p']); 
-        } else {
-          echo "<h1 class='mt-4'>Página em construção</h1>
+      include($_GET['p']);
+    } else {
+      echo "<h1 class='mt-4'>Página em construção</h1>
           <p>Locadora RMGD. Em construção.</p>
           <p>Desculpe o incômodo, voltaremos em breve!</p>";
-        }
-        ?>
-
-
-
-
-          </div>
-=======
->>>>>>> b04faebbda3dfbf23f1f54c713f18817dd0b2b7b
-        </div>
-        <!-- /#page-content-wrapper -->
+    }
+    ?>
+      </div>
 
     </div>
-    <!-- /#wrapper -->
+    <!-- /#page-content-wrapper -->
+
+  </div>
+  <!-- /#wrapper -->
 
 
 
-    <!-- Menu Toggle Script -->
-    <script>
+  <!-- Menu Toggle Script -->
+  <script>
     $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
     });
-    </script>
+  </script>
 </body>
 
 </html>
